9dda126a1c38fb9bd87df7b6ac47001bc1eec5c6
Caio Mori <caiomori22@icloud.com>
2024-08-20 22:47:29 -0300

    feat: Add CoinData.swift file

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,9 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Caio Hideki Martins Alves Mori on 20/08/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
